21be993ce05b7b9c1867c65ac60ccd4abcffbd1f
samridha101 <samridhasuresh5@gmail.com>
2025-05-12 06:51:12 -0400

    Modified emotion detection function

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -11,5 +12,28 @@ def emotion_detector(text_to_analyze):
             "text": text_to_analyze
         }
     }
+
     response = requests.post(url, headers=headers, json=payload)
-    return response.text
+    result = json.loads(response.text)
+
+    emotions = result['emotionPredictions'][0]['emotion']
+    anger = emotions['anger']
+    disgust = emotions['disgust']
+    fear = emotions['fear']
+    joy = emotions['joy']
+    sadness = emotions['sadness']
+
+    scores = {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+    }
+
+    # Identify dominant emotion
+    dominant = max(scores, key=scores.get)
+    scores['dominant_emotion'] = dominant
+
+    return scores
+
